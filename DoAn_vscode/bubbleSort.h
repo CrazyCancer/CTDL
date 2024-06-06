70361ddb855251028655fea6b86c1a403b00a286
desktop-n2nk6f8\crazycancer <dangphuocbao5530@gmail.com>
2024-06-06 11:38:50 +0700

    update

--- DoAn_vscode/bubbleSort.h
+++ DoAn_vscode/bubbleSort.h
@@ -1,8 +1,3 @@
-// int cmpStr(string s1, string s2)
-// {
-//     return strcmp(s1.c_str(), s2.c_str());
-// }
-
 void bubbleSortArray(SV LIST_MANG[], int tieuChi)
 {
     for (int i = 1; i < soLuong; i++)
